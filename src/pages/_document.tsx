137d3347fb268468efb09b77b2a45bd145c6c1cf
DuncanEdwards <duncan.edwards@abide-financial.com>
2023-02-04 19:48:43 +0000

    Initial version

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        <div className="bg-gray-300">
+          <div className=" flex mx-auto max-w-6xl justify-center">
+            <img alt="teletubbies logo" src="/images/teletubbies-logo.png" />
+          </div>
+        </div>
+        <div className=" flex mx-auto max-w-6xl justify-center">
+          <Main />
+        </div>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
